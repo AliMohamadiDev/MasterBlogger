d28961629af470485768eb9ce6205d5713765808
Ali <pachinco.a.m@gmail.com>
2022-07-18 20:03:59 +0430

    Add project files.

---
+++ MB.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MB.Application.Contracts.ArticleCategory;
+
+namespace MB.Domain.ArticleCategoryAgg;
+
+public interface IArticleCategoryRepository
+{
+    List<ArticleCategory> GetAll();
+    ArticleCategory Get(long id);
+    void Add(ArticleCategory entity);
+    void Save();
+    bool Exists(string title);
+}
---
+++ MB.Domain/ArticleCategoryAgg/Services/ArticleCategoryValidatorService.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace MB.Domain.ArticleCategoryAgg.Services;
+
+class ArticleCategoryValidatorService : IArticleCategoryValidatorService
+{
+    private readonly IArticleCategoryRepository _articleCategoryRepository;
+
+    public ArticleCategoryValidatorService(IArticleCategoryRepository articleCategoryRepository)
+    {
+        _articleCategoryRepository = articleCategoryRepository;
+    }
+
+    public void CheckThatThisRecordAlreadyExists(string title)
+    {
+        if (_articleCategoryRepository.Exists(title))
+            throw new DuplicateNameException("This record already exists in database.");
+    }
+}
