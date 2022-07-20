c75464076eaa2ed61ee7ad4857aeffe24f8431d5
Ali <pachinco.a.m@gmail.com>
2022-07-20 20:10:42 +0430

    ArticleDetails Page Implemented

--- MB.Presentation.MVCCore/Pages/Index.cshtml.cs
+++ MB.Presentation.MVCCore/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MB.Infrastructure.Query;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MB.Presentation.MVCCore.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<ArticleQueryView> Articles { get; set; }
+
+        private readonly IArticleQuery _articleQuery;
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IArticleQuery articleQuery)
         {
             _logger = logger;
+            _articleQuery = articleQuery;
         }
 
         public void OnGet()
         {
-
+            Articles = _articleQuery.GetArticles();
         }
     }
 }
